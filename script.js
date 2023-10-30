737d758523bf6545ad100454b2feece93079f384
Brian Werner <brianmw199@gmail.com>
2023-10-30 17:26:49 -0500

    we creeated a ttack function with seitch statements

--- script.js
+++ script.js
@@ -1,7 +1,61 @@
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+
+// HP and AP counters
+let fungusHP = 100;
+let playerAP = 100;
+
+// prevent HP/AP from going below 0
+
+function attack(event) {
+
+    // immediate actions
+    console.log('I\'m attacking!');
+
+    // change HP/AP values based on clicked attack
+    let currentAttack = event.target.getAttribute('class')
+    switch (currentAttack) {
+        case ('attack-btn arcane-scepter'):
+            playerAP -= 12;
+            fungusHP -= 14;
+            break;
+        case('attack-btn entangle'):
+            playerAP -= 23;
+            fungusHP -= 9;
+            break;
+        case('attack-btn dragon-blade'):
+            playerAP -= 38;
+            fungusHP -= 47;
+            break;
+        case('attack-btn star-fire'):
+            playerAP -= 38;
+            fungusHP -= 25;
+            break;
+    }
+
+    // update DOM
+    renderChanges();
+};
+
+
+function renderChanges() {
+
+    // change DOM to reflect updated variables
+    document.getElementById("ap-text").innerText = `${playerAP} AP`
+    document.getElementById("hp-text").innerText = `${fungusHP} HP`
+
+    console.log(fungusHP)
+    // set death states
+    if (playerAP <= 0) {
+        document.getElementById('fungus').classList.remove('walk')
+        document.getElementById('fungus').classList.add('jump')
+    }
+    if (fungusHP <= 0) {
+        document.getElementById('fungus').classList.remove('walk')
+        document.getElementById('fungus').classList.add('dead')
+    }
+}
 
 function onReady() {
     console.log("Ready to go!")
@@ -15,6 +69,4 @@ function onReady() {
     // - Updates state which is ->
     // - Rendered to the DOM
 }
-
-
 onReady()
